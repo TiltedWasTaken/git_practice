3f2bb2ad96349253783a71e2fe1de7a38add2725
TiltedWasTaken <93789904+TiltedWasTaken@users.noreply.github.com>
2021-12-09 17:04:46 +0000

    Made each player move permanently change key value

--- tictactoe.py
+++ tictactoe.py
@@ -1,28 +1,32 @@
 class TicTacToe:
     #Class variables:
+    board_move = {"a1": "a1", "a2": "a2", "a3": "a3", "b1": "b1", 
+    "b2": "b2", "b3": "b3", "c1": "c1", "c2": "c2", "c3": "c3"}
     def __init__(self, player = ""):
         self.player = player    
     def __repr__(self):
         return self.player
+        
     #Board Method
     def board(self, move = ""):
-        board_move = {"a1": " ", "a2": " ", "a3": " ", "b1": " ",
-         "b2": " ", "b3": " ", "c1": " ", "c2": " ", "c3": " "}
+
 
         if move != "" and self.player == "Player_One":
-            board_move[move] = "X"
+            TicTacToe.board_move[move] = " X"
         if move != "" and self.player == "Player_Two":
-            board_move[move] = "O"
-
+            TicTacToe.board_move[move] = " O"
+            
+        print("\n")
         print("   |   |   \n")
-        print(" " + board_move["a1"] + " | " + board_move["a2"] + " | " + board_move["a3"] + " \n")
+        print("" + TicTacToe.board_move["a1"] + " |" + TicTacToe.board_move["a2"] + " |" + TicTacToe.board_move["a3"] + " \n")
         print("_ _|_ _|_ _\n")
         print("   |   |   \n")
-        print(" " + board_move["b1"] + " | " + board_move["b2"] + " | " + board_move["b3"] + " \n")
+        print("" + TicTacToe.board_move["b1"] + " |" + TicTacToe.board_move["b2"] + " |" + TicTacToe.board_move["b3"] + " \n")
         print("_ _|_ _|_ _\n")
         print("   |   |   \n")
-        print(" " + board_move["c1"] + " | " + board_move["c2"] + " | " + board_move["c3"] + " \n")
+        print("" + TicTacToe.board_move["c1"] + " |" + TicTacToe.board_move["c2"] + " |" + TicTacToe.board_move["c3"] + " \n")
         print("   |   |   \n")
+        print("\n")
     
     def move(self, move):
         self.board(move)
@@ -32,4 +36,5 @@ player_one = TicTacToe("Player_One")
 player_two = TicTacToe("Player_Two")
 
 player_one.move("a2")
-
+player_one.move("a1")
+player_one.move("a3")
