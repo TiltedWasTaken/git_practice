764a97432bdc9aeb8aac2763c54040f80a986bf9
TiltedWasTaken <93789904+TiltedWasTaken@users.noreply.github.com>
2021-12-09 23:38:33 +0000

    Added method to check for a win and print the victor

--- tictactoe.py
+++ tictactoe.py
@@ -7,7 +7,8 @@ class TicTacToe:
     def __repr__(self):
         return self.player
     win = False
-    draw = False    
+    draw = False
+    winner = ""    
     #Board Method
     def board(self, move = ""):
 
@@ -31,6 +32,46 @@ class TicTacToe:
     
     def move(self, move):
         self.board(move)
+    def check_win(self):
+        win_con_1 = TicTacToe.board_move["a1"], TicTacToe.board_move["a2"], TicTacToe.board_move["a3"]
+        win_con_2 = TicTacToe.board_move["b1"], TicTacToe.board_move["b2"], TicTacToe.board_move["b3"]
+        win_con_3 = TicTacToe.board_move["c1"], TicTacToe.board_move["c2"], TicTacToe.board_move["c3"]
+        win_con_4 = TicTacToe.board_move["a1"], TicTacToe.board_move["b1"], TicTacToe.board_move["c1"]
+        win_con_5 = TicTacToe.board_move["a2"], TicTacToe.board_move["b2"], TicTacToe.board_move["c2"]
+        win_con_6 = TicTacToe.board_move["a3"], TicTacToe.board_move["b3"], TicTacToe.board_move["c3"]
+        win_con_7 = TicTacToe.board_move["c1"], TicTacToe.board_move["b2"], TicTacToe.board_move["a3"]
+        win_con_8 = TicTacToe.board_move["a1"], TicTacToe.board_move["b2"], TicTacToe.board_move["c3"]
+
+        player_one_win = (" X", " X", " X")
+        player_two_win = (" O", " O", " O")
+        if win_con_1 == player_one_win or win_con_1 == player_two_win:
+            TicTacToe.winner = self.player
+            TicTacToe.win = True
+        if win_con_2 == player_one_win or win_con_2 == player_two_win:
+            TicTacToe.winner = self.player
+            TicTacToe.win = True
+        if win_con_3 == player_one_win or win_con_3 == player_two_win:
+            TicTacToe.winner = self.player
+            TicTacToe.win = True
+        if win_con_4 == player_one_win or win_con_4 == player_two_win:
+            TicTacToe.winner = self.player
+            TicTacToe.win = True
+        if win_con_5 == player_one_win or win_con_5 == player_two_win:
+            TicTacToe.winner = self.player
+            TicTacToe.win = True
+        if win_con_6 == player_one_win or win_con_6 == player_two_win:
+            TicTacToe.winner = self.player
+            TicTacToe.win = True
+        if win_con_7 == player_one_win or win_con_7 == player_two_win:
+            TicTacToe.winner = self.player
+            TicTacToe.wini = True
+        if win_con_8 == player_one_win or win_con_8 == player_two_win:
+            TicTacToe.winner = self.player
+            TicTacToe.win = True   
+        if TicTacToe.winner != "" and TicTacToe.win == True:
+            print(TicTacToe.winner + " wins. \n" + "Game Over!\n")
+
+        
 
     def check_draw(self):
         blank_count = 0
@@ -45,14 +86,8 @@ class TicTacToe:
 player_one = TicTacToe("Player_One")
 player_two = TicTacToe("Player_Two")
 
-player_one.move("a2")
-player_one.move("a1")
-player_one.move("a3")
 player_one.move("b1")
 player_one.move("b2")
 player_one.move("b3")
-player_one.move("c1")
-player_one.move("c2")
-player_one.move("c3")
+player_one.check_win()
 
-player_one.check_draw()
